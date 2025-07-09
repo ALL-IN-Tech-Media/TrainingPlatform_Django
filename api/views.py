22253c71ccc07afc4399a262c1f7b7be7c58ebbb
Zixiao Wang <sw319346@gmail.com>
2025-07-09 18:01:34 +0800

    删除无用的api应用

--- api/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from rest_framework import viewsets
-from .models import NeuralNetwork
-# from .serializers import NeuralNetworkSerializer
-
-# class NeuralNetworkViewSet(viewsets.ModelViewSet):
-#     queryset = NeuralNetwork.objects.all()
-#     serializer_class = NeuralNetworkSerializer
