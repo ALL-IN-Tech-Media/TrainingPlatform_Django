f5a180aadb4751f26e398bb4bb12b24080d3c185
Zixiao Wang <sw319346@gmail.com>
2025-07-09 18:04:07 +0800

    删除无用的neural应用

--- neural_networks/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class NeuralNetwork(models.Model):
-    name = models.CharField(max_length=100)
-    layers = models.JSONField()  # 存储网络层信息
-    created_at = models.DateTimeField(auto_now_add=True)
