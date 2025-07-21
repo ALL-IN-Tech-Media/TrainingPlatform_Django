8daf2a54dec91cf29d89352e4a6b240140e5ed8f
Zixiao Wang <sw319346@gmail.com>
2025-07-21 12:20:48 +0800

    update_url

--- models/urls.py
+++ models/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path("get_task_model_list/", views.get_task_model_list, name="get_task_model_list"),
     path("add_new_model/", views.add_new_model, name="add_new_model"),
+    path("get_model_list/", views.get_model_list, name="get_model_list"),
+    path("cancel_download/", views.cancel_download, name="cancel_download"),
 ]
